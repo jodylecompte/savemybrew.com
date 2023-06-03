f97e8bfde506a33b5a862522fed46b1dcf2b2afd
Jody LeCompte <jody@jodylecompte.com>
2023-06-03 09:30:13 -0500

    Cleanup debug code + fix type errors

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -25,32 +25,15 @@ export default async function handler(
 
   const { id } = data.user!;
 
-  console.log("id: ", id);
-  console.log("handle: ", handle);
-
-  /*
-  const { error: updateError, data: updateData } = await supabase
-  .from("profiles")
-  .update({ handle })
-  .eq("id", `${id}::uuid`)
-  .select();
-*/
-
-  const { data: selectData } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("id", id);
-
-  console.log("selectData: ", selectData);
-
   const { error: updateError, data: updateData } = await supabase
     .from("profiles")
     .update({ handle })
     .eq("id", "" + id)
     .select();
 
-  console.log("updateError: ", updateError);
-  console.log("updateData: ", updateData);
-
-  res.json(data);
+  if (!error) {
+    res.json({ data });
+  } else {
+    res.json({ error: updateError });
+  }
 }
